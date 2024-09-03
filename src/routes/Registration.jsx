5ffc92208a71631a60e9c1b662079e35b39b3ace
Dominic Brian <dominic91960@gmail.com>
2024-09-03 12:37:28 +0530

    Added a responisve Membership Information form.

--- src/routes/Registration.jsx
+++ src/routes/Registration.jsx
@@ -15,8 +15,8 @@ const Registration = () => {
   return (
     <>
       <ImageSection bgMobile={bgMobile} bgDesktop={bgDesktop} />
-      {/* -------- -------- REGISTRATION FORM -------- -------- */}
       <section className="-mb-[8%] bg-white px-[10vw] sm:-mb-[13.5%] lg:-mb-[10%] xl:-mb-[8%] 2xl:-mb-[6%]">
+        {/* -------- -------- PERSONAL INFO FORM -------- -------- */}
         <form
           onSubmit={handleFromSubmit}
           className="hidden -translate-y-[8%] bg-secondary px-4 py-2 sm:-translate-y-[13.5%] sm:px-[12%] sm:py-[5%]"
@@ -131,11 +131,12 @@ const Registration = () => {
           </fieldset>
 
           <div className="flex justify-end pt-[1.4em]">
-            <input
+            <button
               type="submit"
-              value="Next"
               className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]"
-            />
+            >
+              Next
+            </button>
           </div>
 
           <div className="mx-auto flex w-fit items-center pb-4 pt-3 sm:pt-[15%]">
@@ -151,9 +152,10 @@ const Registration = () => {
             <div className="size-[12px] rounded-full border-2 border-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
           </div>
         </form>
+        {/* -------- -------- MEDICAL INFO FORM -------- -------- */}
         <form
           onSubmit={handleFromSubmit}
-          className="-translate-y-[8%] bg-secondary px-4 py-2 sm:-translate-y-[13.5%] sm:px-[12%] sm:py-[5%]"
+          className="hidden -translate-y-[8%] bg-secondary px-4 py-2 sm:-translate-y-[13.5%] sm:px-[12%] sm:py-[5%]"
         >
           <h2 className="text-[13px] font-medium capitalize leading-[2em] text-primary sm:pb-[1em] sm:text-[18px] md:text-[23px] lg:text-[28px] xl:text-[33px] 2xl:text-[38px]">
             Personal information
@@ -208,18 +210,19 @@ const Registration = () => {
             <br />
             <div className="flex gap-x-[20%] py-1">
               <div className="flex items-center gap-x-[30%]">
-                <input type="radio" name="isAllergic" id="yes" value="yes" />
-                <RegistrationLabel
-                  htmlFor="yes"
-                  text="Yes"
-                  type="sub"
+                <input
+                  type="radio"
+                  name="is-allergic"
+                  id="yes"
+                  value="yes"
                   required
                 />
+                <RegistrationLabel htmlFor="yes" text="Yes" type="sub" />
               </div>
               <div className="flex items-center gap-x-[30%]">
                 <input
                   type="radio"
-                  name="isAllergic"
+                  name="is-allergic"
                   id="no"
                   value="no"
                   required
@@ -241,14 +244,18 @@ const Registration = () => {
           </fieldset>
 
           <div className="flex justify-between pt-[1.4em]">
-            <button className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]">
+            <button
+              type="button"
+              className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]"
+            >
               Back
             </button>
-            <input
+            <button
               type="submit"
-              value="Next"
               className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]"
-            />
+            >
+              Next
+            </button>
           </div>
 
           <div className="mx-auto flex w-fit items-center pb-4 pt-3 sm:pt-[15%]">
@@ -259,6 +266,216 @@ const Registration = () => {
             <div className="h-[2px] w-[15px] bg-primary sm:w-[40px] md:w-[50px] lg:w-[60px] xl:w-[70px] 2xl:w-[80px]"></div>
 
             <div className="size-[12px] rounded-full border-2 border-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
+            <div className="h-[2px] w-[15px] bg-primary sm:w-[40px] md:w-[50px] lg:w-[60px] xl:w-[70px] 2xl:w-[80px]"></div>
+
+            <div className="size-[12px] rounded-full border-2 border-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
+          </div>
+        </form>
+        {/* -------- -------- MEMBERSHIP INFO FORM -------- -------- */}
+        <form
+          onSubmit={handleFromSubmit}
+          className="-translate-y-[8%] bg-secondary px-4 py-2 sm:-translate-y-[13.5%] sm:px-[12%] sm:py-[5%]"
+        >
+          <h2 className="text-[13px] font-medium capitalize leading-[2em] text-primary sm:pb-[1em] sm:text-[18px] md:text-[23px] lg:text-[28px] xl:text-[33px] 2xl:text-[38px]">
+            Membership information
+          </h2>
+
+          {/* Membership Type */}
+          <fieldset>
+            <RegistrationLabel text="Choose membership type" />
+            <br />
+            <div className="flex justify-between py-1">
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="membership-type"
+                  id="monthly"
+                  value="monthly"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="monthly"
+                  text="Monthly membership"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="membership-type"
+                  id="annual"
+                  value="annual"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="annual"
+                  text="Annual membership"
+                  type="sub"
+                />
+              </div>
+              <div></div>
+            </div>
+          </fieldset>
+
+          {/* Plan */}
+          <fieldset className="py-[3%]">
+            <RegistrationLabel text="Choose your plan" />
+            <br />
+            <div className="flex justify-between py-1">
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="plan-type"
+                  id="standard-plan"
+                  value="standard-plan"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="standard-plan"
+                  text="Standard"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="plan-type"
+                  id="premium-plan"
+                  value="premium-plan"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="premium-plan"
+                  text="Premium"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="plan-type"
+                  id="platinum"
+                  value="platinum"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="platinum"
+                  text="Platinum"
+                  type="sub"
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          {/* Total Fee */}
+          <RegistrationLabel text="Total fee: $ 35" />
+
+          {/* Trainer */}
+          <fieldset className="pt-[3%]">
+            <RegistrationLabel text="Choose your trainer" />
+            <br />
+            <div className="flex justify-between py-1">
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="trainer"
+                  id="kevin-dias"
+                  value="kevin-dias"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="kevin-dias"
+                  text="Kevin dias"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="trainer"
+                  id="brian-domi"
+                  value="brian-domi"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="brian-domi"
+                  text="Brian domi"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="trainer"
+                  id="shene-lofi"
+                  value="shene-lofi"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="shene-lofi"
+                  text="Shene Lofi"
+                  type="sub"
+                />
+              </div>
+              <div className="flex items-center gap-x-2">
+                <input
+                  type="radio"
+                  name="trainer"
+                  id="alex-guvi"
+                  value="alex-guvi"
+                  required
+                />
+                <RegistrationLabel
+                  htmlFor="alex-guvi"
+                  text="Alex guvi"
+                  type="sub"
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          <fieldset className="pt-[3%]">
+            <RegistrationLabel
+              htmlFor="start-date"
+              text="Preferred start date"
+            />
+            <br />
+            <RegistrationInput type="date" name="start-date" classes="w-2/5" />
+            <br />
+          </fieldset>
+
+          <fieldset className="flex items-center gap-x-2 pb-[50%] pt-[3%]">
+            <input type="checkbox" id="conditions" name="conditions" />
+            <RegistrationLabel
+              htmlFor="conditions"
+              text="I agree to the terms and conditons"
+              type="sub"
+            />
+          </fieldset>
+
+          <div className="flex justify-between pt-[1.4em]">
+            <button
+              type="button"
+              className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]"
+            >
+              Back
+            </button>
+            <button
+              type="submit"
+              className="cursor-pointer bg-primary px-[1em] py-[0.5em] text-[10px] font-semibold capitalize leading-none text-black md:text-[12px] xl:text-[14px] 2xl:text-[16px]"
+            >
+              Next
+            </button>
+          </div>
+
+          <div className="mx-auto flex w-fit items-center pb-4 pt-3 sm:pt-[15%]">
+            <div className="size-[12px] rounded-full border-2 border-primary bg-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
+            <div className="h-[2px] w-[15px] bg-primary sm:w-[40px] md:w-[50px] lg:w-[60px] xl:w-[70px] 2xl:w-[80px]"></div>
+
+            <div className="size-[12px] rounded-full border-2 border-primary bg-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
+            <div className="h-[2px] w-[15px] bg-primary sm:w-[40px] md:w-[50px] lg:w-[60px] xl:w-[70px] 2xl:w-[80px]"></div>
+
+            <div className="size-[12px] rounded-full border-2 border-primary bg-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
             <div className="h-[2px] w-[15px] bg-primary sm:w-[40px] md:w-[50px] lg:w-[60px] xl:w-[70px] 2xl:w-[80px]"></div>
 
             <div className="size-[12px] rounded-full border-2 border-primary sm:size-[16px] md:size-[20px] lg:size-[24px] xl:size-[28px] 2xl:size-[32px]"></div>
