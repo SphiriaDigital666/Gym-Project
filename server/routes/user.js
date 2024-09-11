972ac49384232b36a5049f1e435d0be18f1847cc
Dominic Brian <dominic91960@gmail.com>
2024-09-11 13:43:23 +0530

    Seperated route code from app.js.

---
+++ server/routes/user.js
@@ -0,0 +1,552 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const router = express.Router();
+const User = require("../models/user");
+
+router.get("/registration", (req, res, next) => {
+  // Check whether authorization headers are set
+  if (!req.headers.authorization) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Make sure authorization header is valid
+  const authFragments = req.headers.authorization.split(" ");
+  if (authFragments.length !== 2) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Verify the token
+  const authToken = authFragments[1];
+  let email;
+  try {
+    const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+    email = decodedToken.email;
+    if (!email) {
+      throw new Error("User not found.");
+    }
+  } catch (err) {
+    // we'll send a custom error message if one is set, otherwise we'll send a default message.
+    if (err.message) {
+      return res.json({
+        success: false,
+        error: err.message,
+      });
+    }
+    res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // We'll fetch data for the email extracted from the token
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        throw new Error("Could not find user");
+      }
+
+      const firstName = user.personalInfo.firstName;
+      const lastName = user.personalInfo.lastName;
+
+      res.json({
+        success: true,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+      });
+    })
+    .catch((err) => {
+      console.log("/registration:GET ", err);
+      if (err.message) {
+        return res.json({
+          success: false,
+          error: err.message,
+        });
+      }
+      res.json({
+        success: false,
+        error: "Couldn't verify user",
+      });
+    });
+});
+
+router.post("/registration", (req, res, next) => {
+  // Check whether authorization headers are set
+  if (!req.headers.authorization) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Make sure authorization header is valid
+  const authFragments = req.headers.authorization.split(" ");
+  if (authFragments.length !== 2) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Verify the token
+  const authToken = authFragments[1];
+  let email;
+  try {
+    const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+    email = decodedToken.email;
+    if (!email) {
+      throw new Error("User not found.");
+    }
+  } catch (err) {
+    // we'll send a custom error message if one is set, otherwise we'll send a default message.
+    if (err.message) {
+      return res.json({
+        success: false,
+        error: err.message,
+      });
+    }
+    res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // const newEmail = req.body.personalInfo.email;
+  const personalInfo = {
+    profilePic: "",
+    firstName: req.body.personalInfo.firstName,
+    lastName: req.body.personalInfo.lastName,
+    tel: req.body.personalInfo.tel,
+    DOB: req.body.personalInfo.DOB,
+    gender: req.body.personalInfo.gender,
+    addressOne: req.body.personalInfo.addressOne,
+    addressTwo: req.body.personalInfo.addressTwo,
+    currentWeight: req.body.personalInfo.currentWeight,
+    height: req.body.personalInfo.height,
+    goalWeight: req.body.personalInfo.goalWeight,
+    currentJob: req.body.personalInfo.currentJob,
+  };
+  const emergencyInfo = req.body.emergencyInfo;
+  const medicalInfo = req.body.medicalInfo;
+  const membershipInfo = req.body.membershipInfo;
+
+  // Find a user and update the user. We'll update a dummy user until authentication is implemented
+  User.findOneAndUpdate(
+    { email: email },
+    {
+      // email: newEmail,
+      personalInfo: personalInfo,
+      emergencyInfo: emergencyInfo,
+      medicalInfo: medicalInfo,
+      membershipInfo: membershipInfo,
+    }
+  )
+    .then((result) => {
+      res.json({ success: true });
+    })
+    .catch((err) => {
+      res.json({
+        success: false,
+        error: "Couldn't complete registration. Please try again later",
+      });
+    });
+});
+
+router.get("/profile", (req, res, next) => {
+  // Check whether authorization headers are set
+  if (!req.headers.authorization) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Make sure authorization header is valid
+  const authFragments = req.headers.authorization.split(" ");
+  if (authFragments.length !== 2) {
+    return res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // Verify the token
+  const authToken = authFragments[1];
+  let email;
+  try {
+    const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+    email = decodedToken.email;
+    if (!email) {
+      throw new Error("User not found.");
+    }
+  } catch (err) {
+    // we'll send a custom error message if one is set, otherwise we'll send a default message.
+    if (err.message) {
+      return res.json({
+        success: false,
+        error: err.message,
+      });
+    }
+    res.json({
+      success: false,
+      error: "Couldn't verify user",
+    });
+  }
+
+  // We'll fetch data for the email extracted from the token
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        throw new Error("Could not find user");
+      }
+
+      if (!user.personalInfo.tel) {
+        return res.json({
+          success: true,
+          userInfo: {
+            personalInfo: {
+              fullName: `${user.personalInfo.firstName} ${user.personalInfo.lastName}`,
+              email: user.email,
+            },
+            emergencyInfo: {},
+            medicalInfo: {},
+            membershipInfo: {},
+          },
+        });
+      }
+
+      const personalInfo = {
+        fullName: `${user.personalInfo.firstName} ${user.personalInfo.lastName}`,
+        email: user.email,
+        tel: user.personalInfo.tel,
+        DOB: user.personalInfo.DOB.toDateString(),
+        currentJob: user.personalInfo.currentJob,
+        gender: user.personalInfo.gender,
+        address: user.personalInfo.addressOne,
+        currentWeight: user.personalInfo.currentWeight,
+        height: user.personalInfo.height,
+        goalWeight: user.personalInfo.goalWeight,
+      };
+      const emergencyInfo = {
+        emergencyFullName: `${user.emergencyInfo.emergencyFirstName} ${user.emergencyInfo.emergencyLastName}`,
+        emergencyTel: user.emergencyInfo.emergencyTel,
+        emergencyRelationship: user.emergencyInfo.emergencyRelationship,
+      };
+      const medicalInfo = {
+        allergyDetails:
+          user.medicalInfo.allergyDetails === ""
+            ? "No"
+            : user.medicalInfo.allergyDetails,
+      };
+      const membershipInfo = {
+        membershipType: user.membershipInfo.membershipType,
+        planType: user.membershipInfo.planType,
+        startDate: user.membershipInfo.startDate.toDateString(),
+        trainer: user.membershipInfo.trainer,
+      };
+      res.json({
+        success: true,
+        userInfo: {
+          personalInfo: personalInfo,
+          emergencyInfo: emergencyInfo,
+          medicalInfo: medicalInfo,
+          membershipInfo: membershipInfo,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log("/profile:GET ", err);
+      if (err.message) {
+        return res.json({
+          success: false,
+          error: err.message,
+        });
+      }
+      res.json({
+        success: false,
+        error: "Couldn't verify user",
+      });
+    });
+});
+
+module.exports = router;
+// app.get("/registration", (req, res, next) => {
+//   // Check whether authorization headers are set
+//   if (!req.headers.authorization) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Make sure authorization header is valid
+//   const authFragments = req.headers.authorization.split(" ");
+//   if (authFragments.length !== 2) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Verify the token
+//   const authToken = authFragments[1];
+//   let email;
+//   try {
+//     const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+//     email = decodedToken.email;
+//     if (!email) {
+//       throw new Error("User not found.");
+//     }
+//   } catch (err) {
+//     // we'll send a custom error message if one is set, otherwise we'll send a default message.
+//     if (err.message) {
+//       return res.json({
+//         success: false,
+//         error: err.message,
+//       });
+//     }
+//     res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // We'll fetch data for the email extracted from the token
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (!user) {
+//         throw new Error("Could not find user");
+//       }
+
+//       const firstName = user.personalInfo.firstName;
+//       const lastName = user.personalInfo.lastName;
+
+//       res.json({
+//         success: true,
+//         firstName: firstName,
+//         lastName: lastName,
+//         email: email,
+//       });
+//     })
+//     .catch((err) => {
+//       console.log("/registration:GET ", err);
+//       if (err.message) {
+//         return res.json({
+//           success: false,
+//           error: err.message,
+//         });
+//       }
+//       res.json({
+//         success: false,
+//         error: "Couldn't verify user",
+//       });
+//     });
+// });
+
+// app.get("/profile", (req, res, next) => {
+//   // Check whether authorization headers are set
+//   if (!req.headers.authorization) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Make sure authorization header is valid
+//   const authFragments = req.headers.authorization.split(" ");
+//   if (authFragments.length !== 2) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Verify the token
+//   const authToken = authFragments[1];
+//   let email;
+//   try {
+//     const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+//     email = decodedToken.email;
+//     if (!email) {
+//       throw new Error("User not found.");
+//     }
+//   } catch (err) {
+//     // we'll send a custom error message if one is set, otherwise we'll send a default message.
+//     if (err.message) {
+//       return res.json({
+//         success: false,
+//         error: err.message,
+//       });
+//     }
+//     res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // We'll fetch data for the email extracted from the token
+//   User.findOne({ email: email })
+//     .then((user) => {
+//       if (!user) {
+//         throw new Error("Could not find user");
+//       }
+
+//       if (!user.personalInfo.tel) {
+//         return res.json({
+//           success: true,
+//           userInfo: {
+//             personalInfo: {
+//               fullName: `${user.personalInfo.firstName} ${user.personalInfo.lastName}`,
+//               email: user.email,
+//             },
+//             emergencyInfo: {},
+//             medicalInfo: {},
+//             membershipInfo: {},
+//           },
+//         });
+//       }
+//       const personalInfo = {
+//         fullName: `${user.personalInfo.firstName} ${user.personalInfo.lastName}`,
+//         email: user.email,
+//         tel: user.personalInfo.tel,
+//         DOB: user.personalInfo.DOB.toDateString(),
+//         currentJob: user.personalInfo.currentJob,
+//         gender: user.personalInfo.gender,
+//         address: user.personalInfo.addressOne,
+//         currentWeight: user.personalInfo.currentWeight,
+//         height: user.personalInfo.height,
+//         goalWeight: user.personalInfo.goalWeight,
+//       };
+//       console.log(personalInfo);
+
+//       const emergencyInfo = {
+//         emergencyFullName: `${user.emergencyInfo.emergencyFirstName} ${user.emergencyInfo.emergencyLastName}`,
+//         emergencyTel: user.emergencyInfo.emergencyTel,
+//         emergencyRelationship: user.emergencyInfo.emergencyRelationship,
+//       };
+//       const medicalInfo = {
+//         allergyDetails:
+//           user.medicalInfo.allergyDetails === ""
+//             ? "No"
+//             : user.medicalInfo.allergyDetails,
+//       };
+//       const membershipInfo = {
+//         membershipType: user.membershipInfo.membershipType,
+//         planType: user.membershipInfo.planType,
+//         startDate: user.membershipInfo.startDate.toDateString(),
+//         trainer: user.membershipInfo.trainer,
+//       };
+//       res.json({
+//         success: true,
+//         userInfo: {
+//           personalInfo: personalInfo,
+//           emergencyInfo: emergencyInfo,
+//           medicalInfo: medicalInfo,
+//           membershipInfo: membershipInfo,
+//         },
+//       });
+//     })
+//     .catch((err) => {
+//       console.log("/profile:GET ", err);
+//       if (err.message) {
+//         return res.json({
+//           success: false,
+//           error: err.message,
+//         });
+//       }
+//       res.json({
+//         success: false,
+//         error: "Couldn't verify user",
+//       });
+//     });
+// });
+
+// app.post("/registration", (req, res, next) => {
+//   // Check whether authorization headers are set
+//   if (!req.headers.authorization) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Make sure authorization header is valid
+//   const authFragments = req.headers.authorization.split(" ");
+//   if (authFragments.length !== 2) {
+//     return res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // Verify the token
+//   const authToken = authFragments[1];
+//   let email;
+//   try {
+//     const decodedToken = jwt.verify(authToken, process.env.JWT_KEY);
+//     email = decodedToken.email;
+//     if (!email) {
+//       throw new Error("User not found.");
+//     }
+//   } catch (err) {
+//     // we'll send a custom error message if one is set, otherwise we'll send a default message.
+//     if (err.message) {
+//       return res.json({
+//         success: false,
+//         error: err.message,
+//       });
+//     }
+//     res.json({
+//       success: false,
+//       error: "Couldn't verify user",
+//     });
+//   }
+
+//   // const newEmail = req.body.personalInfo.email;
+//   const personalInfo = {
+//     profilePic: "",
+//     firstName: req.body.personalInfo.firstName,
+//     lastName: req.body.personalInfo.lastName,
+//     tel: req.body.personalInfo.tel,
+//     DOB: req.body.personalInfo.DOB,
+//     gender: req.body.personalInfo.gender,
+//     addressOne: req.body.personalInfo.addressOne,
+//     addressTwo: req.body.personalInfo.addressTwo,
+//     currentWeight: req.body.personalInfo.currentWeight,
+//     height: req.body.personalInfo.height,
+//     goalWeight: req.body.personalInfo.goalWeight,
+//     currentJob: req.body.personalInfo.currentJob,
+//   };
+//   const emergencyInfo = req.body.emergencyInfo;
+//   const medicalInfo = req.body.medicalInfo;
+//   const membershipInfo = req.body.membershipInfo;
+
+//   // Find a user and update the user. We'll update a dummy user until authentication is implemented
+//   User.findOneAndUpdate(
+//     { email: email },
+//     {
+//       // email: newEmail,
+//       personalInfo: personalInfo,
+//       emergencyInfo: emergencyInfo,
+//       medicalInfo: medicalInfo,
+//       membershipInfo: membershipInfo,
+//     }
+//   )
+//     .then((result) => {
+//       console.log(result);
+//       res.json({ success: true });
+//     })
+//     .catch((err) => {
+//       res.json({
+//         success: false,
+//         error: "Couldn't complete registration. Please try again later",
+//       });
+//     });
+// });
